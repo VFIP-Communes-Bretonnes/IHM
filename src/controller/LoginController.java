4f1389af4e53a40bfe581a0b85b42c010d38f17b
clement-dionne <dionne.clement@ik.me>
2024-06-17 14:07:49 +0200

    update

---
+++ src/controller/LoginController.java
@@ -0,0 +1,103 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.data.User;
+
+public class LoginController {
+    
+    /**
+     * Private attribute, the button for the login page, a JavaFX Button.
+     */
+    @FXML private Button button_login_connexion;
+    
+    /**
+     * Private attribute, the button for the register page on the login page, a JavaFX Button.
+     */
+    @FXML private Button button_register_connexion;
+    
+    /**
+     * Private attribute, the text field for the username on the login page, a JavaFX TextField.
+     */
+    @FXML private TextField textfield_username_connexion;
+    
+    /**
+     * Private attribute, the text field for the password on the login page, a JavaFX TextField.
+     */
+    @FXML private TextField textfield_psswrd_connexion;
+
+     /**
+     * Public method that tries to log in the user with the specified username and password.
+     * If the login is successful, the admin page is opened.
+     * If the login is unsuccessful, an error message is displayed.
+     *
+     * @param event The ActionEvent that triggered the method. It cannot be null, an ActionEvent object.
+     * @throws RuntimeException if event is null.
+     */
+    public void tryLogin(ActionEvent event){
+        System.out.println("tryLogin");
+        User user = new User();
+        String username = textfield_username_connexion.getText();
+        String password = textfield_psswrd_connexion.getText();
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        if(username == null){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur ne doit pas être null.");
+        }
+        else if(username.isEmpty()){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur ne doit pas être vide.");
+        }
+        else if(username.length() < 3 || username.length() > 20){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur doit comporter entre 3 et 20 caractères.");
+        }
+        else if(password.length() < 8 || password.length() > 32){
+            showPopupInfo(stage, "Le mot de passe est invalide\nLe mot de passe doit comporter entre 8 et 32 caractères.");
+        }
+        else if(!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+            showPopupInfo(stage, "Le mot de passe est invalide\nLe mot de passe doit comporter au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.");
+        }
+        else{
+            if(user.checkLogin(username, password)){
+                try{
+                    System.out.println("openAdminPage");
+                    Scene registerPage = FXMLLoader.load(getClass().getResource("/FXML/AdminScene.fxml"));
+                    stage.setScene(registerPage);
+                    user.saveUserObject();
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else{
+                System.err.println("login err for" + username);
+                showPopupInfo(stage, "Erreur de connexion\nLe mot de passe ou le nom d'utilisateur est invalide.");
+            }
+        }
+    }
+
+    /**
+     * Public method that opens the register page.
+     *
+     * @param event The ActionEvent that triggered the method. It cannot be null, an ActionEvent object.
+     * @throws RuntimeException if event is null.
+     */
+    public void openRegisterPage(ActionEvent event){
+        try{
+            System.out.println("openRegisterPage");
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene registerPage = FXMLLoader.load(getClass().getResource("/FXML/RegisterScene.fxml"));
+            stage.setScene(registerPage);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/controller/RegisterController.java
@@ -0,0 +1,111 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.data.User;
+
+public class RegisterController {
+    
+    /**
+     * Private attribute, the button for the register page, a JavaFX Button.
+     */
+    @FXML private Button button_register_register;
+    
+    /**
+     * Private attribute, the button for the back page on the register page, a JavaFX Button.
+     */
+    @FXML private Button button_back_register;
+    
+    /**
+     * Private attribute, the text field for the username on the register page, a JavaFX TextField.
+     */
+    @FXML private TextField textfield_username_register;
+    
+    /**
+     * Private attribute, the text field for the password on the register page, a JavaFX TextField.
+     */
+    @FXML private TextField textfield_psswrd_register;
+    
+    /**
+     * Private attribute, the text field for the confirm password on the register page, a JavaFX TextField.
+     */
+    @FXML private TextField textfield_psswrd_confirm_register;
+
+    @FXML private CheckBox checkbox_cgu_register;
+    @FXML private DatePicker datepicker_datenaissance_register;
+    @FXML private TextField textfield_mail_register;
+    @FXML private TextField textfield_phone_register;
+
+    
+    /**
+     * Public method that tries to register the user with the specified username, password, and confirm password.
+     * If the registration is successful, a success message is displayed.
+     * If the registration is unsuccessful, an error message is displayed.
+     *
+     * @param event The ActionEvent that triggered the method. It cannot be null, an ActionEvent object.
+     * @throws RuntimeException if event is null.
+     */
+    public void tryRegister(ActionEvent event){
+        System.out.println("tryRegister");
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        String username = textfield_username_register.getText();
+        String mail = textfield_mail_register.getText();
+        String phone = textfield_phone_register.getText();
+        Boolean cgu = checkbox_cgu_register.isSelected();
+        String password = textfield_psswrd_register.getText();
+        String password_confirm = textfield_psswrd_confirm_register.getText();
+
+        if(username == null){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur ne doit pas être null.");
+        }
+        else if(username.isEmpty()){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur ne doit pas être vide.");
+        }
+        else if(username.length() < 3 || username.length() > 20){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur doit comporter entre 3 et 20 caractères.");
+        }
+        else if (!username.matches("^[a-zA-Z0-9-]+$")){
+            showPopupInfo(stage, "Le nom d'utilisateur est invalide\nLe nom d'utilisateur doit comporter uniquement des lettres, des chiffres et des tirets.");
+        }
+        else if(!mail.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")){
+            showPopupInfo(stage, "L'email est invalide\nVeuillez entrer un email valide.");
+        }
+        else if(!phone.matches("^[0-9]{10}$")){
+            showPopupInfo(stage, "Le numéro de téléphone est invalide\nVeuillez entrer un numéro de téléphone valide de 10 chiffres.");
+        }
+        else if(!cgu){
+            showPopupInfo(stage, "Vous devez accepter les conditions générales d'utilisation.");
+        }
+        else if(password.length() < 8 || password.length() > 32){
+            showPopupInfo(stage, "Le mot de passe est invalide\nLe mot de passe doit comporter entre 8 et 32 caractères.");
+        }
+        else if(!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+            showPopupInfo(stage, "Le mot de passe est invalide\nLe mot de passe doit comporter au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.");
+        }
+        else if(!password_confirm.toString().equals(password.toString())){
+            showPopupInfo(stage, "La verification de mot de passe est invalide\nLe mot de passe et la confirmation du mot de passe doivent être identique.");
+        }
+        else{
+            boolean wantToContinue = showPopupYoN(stage, "Vous êtes sur le point de créer l'utilisateur '" + username + "'.\nVouslez vous continuer cette action ?");
+        
+            if (wantToContinue) {
+                if(User.register(username, password, mail, phone)){
+                    showPopupInfo(stage, "compte utilisateur '" + username + "' bien créer !");
+                }
+                else{
+                    showPopupInfo(stage, "Erreur de création de compte\nLa création de l'utilisateur '" + username + "' à échouer !");
+                }
+            }
+            else {
+                System.out.println("Création annulé !");
+            }
+        }
+    }   
+}
